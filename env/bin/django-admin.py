742c10a43cf7fbd373fb2aa7303cefab367d34c5
Ode <odedahay@yahoo.com>
2020-10-04 00:37:00 +0800

    created apps for Carzone

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/odedahay/Documents/Projects/django-projects/car_dealer/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
